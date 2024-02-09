f48df6410637a8643506b346d558074cd1eea8dc
remi-boivin <remi.boivin@epitech.eu>
2024-02-09 14:08:55 -0500

    :sparkles: feat(logger): Implement advanced logging system
    Implemented a comprehensive logging system utilizing the Level enum for defining log severity.
    
    - Added Level enum with DEBUG, WARNING, and ERROR levels.
    - Modified Log class to include log level comparison and name printing.
    
    This update enhances diagnostic capabilities and streamlines troubleshooting processes, ensuring that log output is both informative and user-friendly.

--- src/utilities/log/log.py
+++ src/utilities/log/log.py
@@ -3,10 +3,22 @@
 # Created on: January 22, 2024
 # Last updated on: January 22, 2024# Author(s): remi-boivin
 
-from ..utilities.level import Level
+from ..level import Level
+import inspect
 
 
 class Log:
 
-    def __init__(self, log_level=Level.Error) -> None:
+    def __init__(self, log_level=Level.ERROR) -> None:
         self.log_level = log_level
+
+    def log(self, buff, level=Level.ERROR) -> None:
+        if level.value >= self.log_level.value:
+            file_called = inspect.stack()[1][1].split('/')[9]
+            print(f"[{level.name}]:[{file_called}.py]: {buff}")
+
+    def set_log(self, log_level) -> None:
+        self.log_level = log_level
+
+    def get_log_level(self) -> Level:
+        return self.log_level
