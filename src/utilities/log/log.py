44d6510c6f7f0df734d1b06a02e92ede9d996ede
remi-boivin <remi.boivin@epitech.eu>
2024-02-13 00:27:19 -0500

    :recycle: refactor(log): Add timestamp
    We added the timestamp for better traceability over time.
    
    BREAKING CHANGE: we rename the set_log methon into set_log_level.

--- src/utilities/log/log.py
+++ src/utilities/log/log.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # log.py
 # Created on: January 22, 2024
-# Last updated on: January 22, 2024# Author(s): remi-boivin
 
-from ..level import Level
 import inspect
+from datetime import datetime
+from ..level import Level
 
 
 class Log:
@@ -14,10 +14,13 @@ class Log:
 
     def log(self, buff, level=Level.ERROR) -> None:
         if level.value >= self.log_level.value:
-            file_called = inspect.stack()[1][1].split('/')[9]
-            print(f"[{level.name}]:[{file_called}.py]: {buff}")
+            # Retrieve the file name where the log instance is called
+            file_called = inspect.stack()[1].filename.split('/')[-1]
+            now = datetime.now()
+            timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{timestamp}:[{level.name}]:[{file_called}]: {buff}")
 
-    def set_log(self, log_level) -> None:
+    def set_log_level(self, log_level) -> None:
         self.log_level = log_level
 
     def get_log_level(self) -> Level:
